7f387f115965fd7ef1a6f35b17f9372eca1e90c2
Joaquim Das <71024940+jqm-das@users.noreply.github.com>
2022-12-21 01:08:09 -0500

    Add files via upload

---
+++ regex3.py
@@ -0,0 +1,19 @@
+import sys; args = sys.argv[1:]
+idx = int(args[0])-50
+
+myRegexLst = [
+  r"/\w*(\w)\w*\1\w*/i",
+  r"/\w*(\w)(\w*\1){3}\w*/i",
+  r"/^(0|1)([01]*\1)*$/",
+  r"/\b(?=\w*cat)\w{6}\b/i",
+  r"/\b(?=\w*bri)(?=\w*ing)\w{5,9}\b/i",
+  r"/\b(?!\w*cat)\w{6}\b/i",
+  r"/\b((\w)(?!\w*\2))+\b/i",
+  r"/^((?!10011)[01])*$/",
+  r"/\b(\w*([aeiou])(?!\2)[aeiou])\w*\b/i",
+  r"/^((?!(101|111))[01])*$/"]
+
+if idx < len(myRegexLst):
+  print(myRegexLst[idx])
+
+#Joaquim Das 3 2022
